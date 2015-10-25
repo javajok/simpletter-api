b208522d9128d9766e9396b575556846299aaf29
Taichi Uragami <backpaper0@gmail.com>
2015-10-25 16:30:57 +0900

    サンプルデータを最初に登録する

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,15 +2,23 @@ package app;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    TweetService tweetService;
+    @Autowired
+    AccountService accountService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -20,5 +28,17 @@ public class Application {
     ObjectMapper objectMapper() {
         return Jackson2ObjectMapperBuilder.json().serializerByType(
                 LocalDateTime.class, new LocalDateTimeJsonSerializer()).build();
+    }
+
+    @Bean
+    ApplicationListener<ContextRefreshedEvent> listener() {
+        return new ApplicationListener<ContextRefreshedEvent>() {
+
+            @Override
+            public void onApplicationEvent(ContextRefreshedEvent event) {
+                accountService.signUp("hogekun");
+                tweetService.tweet("hogekun", "こんにちは！");
+            }
+        };
     }
 }
